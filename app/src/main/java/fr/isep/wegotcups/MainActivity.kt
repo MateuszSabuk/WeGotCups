a35df62d2ef38ef50fb3639ddc10942e0c3b2268
mati <mateusz.sabuk@eleve.isep.fr>
2022-12-14 10:33:25 +0100

    made automatic login and added logout in the profile tab

--- app/src/main/java/fr/isep/wegotcups/MainActivity.kt
+++ app/src/main/java/fr/isep/wegotcups/MainActivity.kt
@@ -1,11 +1,14 @@
 package fr.isep.wegotcups
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import fr.isep.wegotcups.databinding.ActivityMainBinding
 import fr.isep.wegotcups.loginregister.LoginFragment
+import fr.isep.wegotcups.loginregister.LoginRegisterActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,7 +38,7 @@ class MainActivity : AppCompatActivity() {
                         true
                     }
                     R.id.item_4 -> {
-                        loadFragment(LoginFragment())
+                        loadFragment(ProfileFragment())
                         true
                     }
                     else -> false
@@ -48,5 +51,14 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.main_fragment_container,fragment)
         transaction.commit()
         }
+
+    fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        startActivity(Intent(this, LoginRegisterActivity::class.java).apply {
+            // you can add values(if any) to pass to the next class or avoid using `.apply`
+            putExtra("doLogout", true)
+        })
+        finish()
+    }
 
 }
