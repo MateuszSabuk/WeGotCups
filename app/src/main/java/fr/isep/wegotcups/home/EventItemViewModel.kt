0a79cc54c06db0d85ea5f51aa5f1eab042478231
mati <mateusz.sabuk@eleve.isep.fr>
2023-01-12 10:31:37 +0100

    Change the display of the hour

--- app/src/main/java/fr/isep/wegotcups/home/EventItemViewModel.kt
+++ app/src/main/java/fr/isep/wegotcups/home/EventItemViewModel.kt
@@ -1,7 +1,5 @@
 package fr.isep.wegotcups.home
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import fr.isep.wegotcups.R
 import fr.isep.wegotcups.databasehandler.EventData
 import java.text.SimpleDateFormat
@@ -10,6 +8,6 @@ import java.util.*
 data class EventItemViewModel(var event: EventData){
     var image = R.drawable.event_cover
     var name: String = event.name.toString()
-    private val displayDateFormat = SimpleDateFormat("dd/MMMM/yyyy hh:mm", Locale.getDefault())
+    private val displayDateFormat = SimpleDateFormat("dd/MMMM/yyyy HH:mm", Locale.getDefault())
     var dateAndTime = displayDateFormat.format(Date(event.datetime.seconds * 1000).time).toString()
 }
